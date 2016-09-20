5dd8f65f14f240fe8dc511828f923e7a17d30fa6
David Yunker <david.yunker@gmail.com>
2016-09-20 11:10:08 -0500

    weather button and page are working

---
+++ app/controllers/WeatherCtrl.js
@@ -0,0 +1,42 @@
+"use strict";
+
+app.controller("WeatherCtrl", function ($scope, $window, AuthFactory, FirebaseFactory, FBCreds, $http, $routeParams, $location) {
+
+
+$scope.weatherInfo = {
+    city: "",
+    state: "",
+    location: "",
+    weather: "",
+    temp: "",
+    feelslike: "",
+    icon: ""
+}
+
+$scope.buttonClicked = false;
+
+
+   $scope.goToAllRoutes = () => {
+    $location.url(`/allroutes`);
+  };
+
+ $scope.loadWeatherInfo = function() {
+      console.log("loadWeatherInfo has started!")
+      FirebaseFactory.getWeatherInfo($scope.weatherInfo)
+        .then(function(data){
+            console.log("it worked!")
+          console.log("data", data)
+          $scope.weatherInfo.location = data.current_observation.display_location.full
+          $scope.weatherInfo.weather = data.current_observation.weather
+          $scope.weatherInfo.temp = data.current_observation.temp_f
+          $scope.weatherInfo.feelslike = data.current_observation.feelslike_f
+          $scope.weatherInfo.icon = data.current_observation.icon_url
+
+        })
+        .then(function() {
+          console.log('TESTING THINGS', $scope.weatherInfo);
+          $scope.buttonClicked = true;
+
+      })
+    }
+})
